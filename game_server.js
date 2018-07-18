1969b8b004b5f6143de74f0e518094c85a6edfc0
Dong Yuwei <newdongyuwei@gmail.com>
2018-07-18 22:49:48 +0800

    implement game server

---
+++ game_server.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+const { GuessNumberGameEngine } = require("./guess_number_game_engine");
+
+const GameServer = {
+  init: function() {
+    this.engine = new GuessNumberGameEngine();
+    return this;
+  },
+  startGameEventLoop: function() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    
+    console.log("Guess Number Game, You have 6 chances to guess!");
+    rl.on('line', (input) => {
+      this.engine.feed(input);
+      this.engine.startGame();
+    });
+  }
+};
+
+
+GameServer.init().startGameEventLoop();
